e993132326d7a4e145f8b13fc87e75adcfe8d479
nrvzRed <93291846+nrvzRed@users.noreply.github.com>
2022-04-24 13:22:30 -0500

    Inicio

---
+++ php/inProducto.php
@@ -0,0 +1,16 @@
+<?php include('../database/conn.php'); 
+  //if(isset($conn)) {
+    //echo 'DB is connected';
+  //}
+?>
+<?php include('../includes/header.php'); ?>
+<div><br><br></div>
+<div class="container">
+    <h1>HOla</h1>
+</div>
+
+
+
+
+
+<?php include('../includes/footer.php'); ?>
